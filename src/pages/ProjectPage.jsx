3cd91ddd04f007b46c14e07faf87292afa8cb97e
Kubraf96 <kubrafidan25@hotmail.com>
2026-08-07 16:44:36 +0200

    Add Navbar to ProjectPage

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,4 +1,5 @@
 import { Link, useParams } from "react-router";
+import Navbar from "../components/Navbar";
 import projects from "../data/projects";
 
 function ProjectPage() {
@@ -7,48 +8,54 @@ function ProjectPage() {
 
   if (!project) {
     return (
-      <div className="page narrow">
-        <p className="eyebrow">404</p>
-        <h1>Projektet blev ikke fundet</h1>
-        <p>Det projekt findes ikke i listen endnu.</p>
-        <Link className="button" to="/projects">
-          Tilbage til projekter
-        </Link>
-      </div>
+      <>
+        <Navbar />
+        <div className="page narrow">
+          <p className="eyebrow">404</p>
+          <h1>Projektet blev ikke fundet</h1>
+          <p>Det projekt findes ikke i listen endnu.</p>
+          <Link className="button" to="/projects">
+            Tilbage til projekter
+          </Link>
+        </div>
+      </>
     );
   }
 
   return (
-    <article className="page narrow">
-      <Link className="back-link" to="/projects">
-        Tilbage til projekter
-      </Link>
+    <>
+      <Navbar />
+      <article className="page narrow">
+        <Link className="back-link" to="/projects">
+          Tilbage til projekter
+        </Link>
 
-      <img className="detail-image" src={project.image} alt="" />
-      <p className="eyebrow">{project.year}</p>
-      <h1>{project.title}</h1>
-      <p className="lead">{project.description}</p>
+        <img className="detail-image" src={project.image} alt="" />
+        <p className="eyebrow">{project.year}</p>
+        <h1>{project.title}</h1>
+        <p className="lead">{project.description}</p>
 
-      <ul className="tag-list">
-        {project.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
+        <ul className="tag-list">
+          {project.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
 
-      <div className="actions">
-        {project.links.map((link) => (
-          <a
-            className="button secondary"
-            href={link.href}
-            key={link.href}
-            rel="noreferrer"
-            target="_blank"
-          >
-            {link.label}
-          </a>
-        ))}
-      </div>
-    </article>
+        <div className="actions">
+          {project.links.map((link) => (
+            <a
+              className="button secondary"
+              href={link.href}
+              key={link.href}
+              rel="noreferrer"
+              target="_blank"
+            >
+              {link.label}
+            </a>
+          ))}
+        </div>
+      </article>
+    </>
   );
 }
 
