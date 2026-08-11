168ca1446c8f3b87c9a1202f4923c9e44110c4ce
Kubraf96 <kubrafidan25@hotmail.com>
2026-08-11 18:28:08 +0200

    changed project info

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,6 +2,7 @@ import controllogoImg from "../assets/productimages/control_logo.svg";
 import controlImg from "../assets/productimages/Control.png";
 import lastOfUsImg from "../assets/productimages/LastOfUs_poster_preview.jpg";
 import surrealismImg from "../assets/productimages/Surrealism_Collage.jpg";
+import twoGatherLogo from "../assets/productimages/2gather/2gather_logo.svg";
 
 const projects = [
   {
@@ -39,12 +40,42 @@ const projects = [
   },
   {
     slug: "last-of-us",
-    title: "Last of Us poster",
-    year: "2025",
-    summary: "Et visuelt projekt med stærke farver og filmisk komposition.",
-    description:
-      "I denne case arbejdede jeg med double exposure i Adobe Photoshop for at skabe en poster inspireret af spillet The Last of Us. Fokus var på at skabe et sammenhængende visuelt udtryk, der vækker nysgerrighed og understøtter fortællingen.",
-    tags: ["Double Exposure", "Adobe Photoshop", "Visuel hierarki", "Storytelling" ],
+    title: "The Last of Us",
+    subtitle: "Game poster",
+    year: "2026",
+    summary:
+      "En alternativ plakat til spillet The Last of Us med fokus på stemning og visuel fortælling.",
+    description: [
+      "I dette projekt skabte jeg en plakat til spilserien The Last of Us ved hjælp af double exposure-teknikken. Jeg ville visualisere forbindelsen mellem karakteren Ellie og den verden, hun bevæger sig igennem - med fokus på stemning, fortælling og komposition.",
+
+      "Projektet gav mig mulighed for at arbejde med billedmanipulation, lagmasker, blending og farvejustering for at få flere billeder til at smelte sammen til én samlet fortælling. Processen var iterativ, og jeg gik løbende tilbage og justerede tidligere elementer, efterhånden som nye lag blev tilføjet.",
+
+      "Jeg er især interesseret i, hvordan man kan fortælle noget om en karakter og en historie uden at bruge ord.",
+    ],
+    tools: ["Adobe Photoshop"],
+    tags: ["Double Exposure", "Visuel hierarki", "Storytelling", "Compositing"],
+    process: [
+      {
+        title: "Idé og research",
+        description:
+          "Jeg arbejdede ud fra spillets stemning og visuelle univers og samlede forskellige referencebilleder til forgrund, mellemgrund og baggrund.",
+      },
+      {
+        title: "Grundlag",
+        description:
+          "Jeg fritlagde portrætter af Ellie som fundament for kompositionen og begyndte at bygge billedets struktur op omkring hende.",
+      },
+      {
+        title: "Double exposure",
+        description:
+          "Jeg benyttede double exposure-metoden og byggede scenen op med flere billedlag og integrerede mellemgrund og baggrund i kompositionen.",
+      },
+      {
+        title: "Farve, lys og finpudsning",
+        description:
+          "Jeg arbejdede løbende med pensler, justeringlag, lys, farver og kontrast for at skabe sammenhæng mellem elementerne og fastholde Ellie som det visuelle fokus.",
+      },
+    ],
     image: lastOfUsImg,
     links: [
       {
@@ -65,6 +96,22 @@ const projects = [
     links: [
       {
         label: "Se billede",
+        href: "#",
+      },
+    ],
+  },
+  {
+    slug: "2gather-event-app",
+    title: "2GATHER event app",
+    year: "2026",
+    summary: "Event-app til at samle venner og organisere arrangementer.",
+    description:
+      "2GATHER er en event-app med chat, invitationer og bekræftelse af deltagelse. Dette er et eksempelprojekt med logo-thumbnail.",
+    tags: ["React", "Events", "Chat"],
+    image: twoGatherLogo,
+    links: [
+      {
+        label: "Se projekt",
         href: "#",
       },
     ],
